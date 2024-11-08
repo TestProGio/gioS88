97358a4d13d161bf6547209f2aea08dc5862b832
TestProGio <giovanna.silva@testpro.io>
2024-11-08 11:59:56 -0500

    AddCountToLastestPlayed

--- src/test/java/apiKoel/RecentlyPlayedSongsTest.java
+++ src/test/java/apiKoel/RecentlyPlayedSongsTest.java
@@ -1,85 +1,6 @@
-/*package apiKoel;
-
-import apiKoel.responses.RecentlyPlayedSongsResponse;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
-
-import static io.restassured.RestAssured.given;
-
-public class RecentlyPlayedSongsTest {
-
-    private String API_TOKEN;
-    private static final String BASE_URL = "https://qa.koel.app";
-
-
-    @BeforeClass
-    public void setup() {
-        // Create an instance of KoelLogin and retrieve the authentication token
-        KoelLogin login = new KoelLogin();
-        API_TOKEN = login.koelLogin();
-        Assert.assertNotNull(API_TOKEN, "Auth token should not be null");
-
-        // Ensure RestAssured is properly configured with the base URL
-        RestAssured.baseURI = BASE_URL;
-        RestAssured.basePath = "/api/interaction";
-    }
-
-    @Test
-    public void testGetRecentlyPlayedSongs() {
-        SoftAssert softAssert = new SoftAssert();
-
-        int count = 1; // Set the number of songs to be returned, if required.
-
-        // Perform GET request
-        Response response = given()
-                .header("Authorization", "Bearer " + API_TOKEN )
-                .header("Accept", "application/json")
-                .header("Content-Type", "application/json")
-                .queryParam("count", count)
-                .when()
-                .get("/recently-played/" + count)
-                .then()
-                .statusCode(200)
-                .extract()
-                .response();
-
-        // Log the JSON response (equivalent to console.log(json))
-        // Debugging output for validation
-        System.out.println("Response Status Code: " + response.statusCode());
-        System.out.println("Response Body: " + response.asString());
-
-        // Deserialize response to POJO
-        RecentlyPlayedSongsResponse recentlyPlayedSongs = response.as(RecentlyPlayedSongsResponse.class);
-
-        // Soft assertions
-        softAssert.assertNotNull(recentlyPlayedSongs.getSongIds(), "Response song list should not be null");
-        if (recentlyPlayedSongs.getSongIds().isEmpty()) {
-            softAssert.assertTrue(recentlyPlayedSongs.getSongIds().isEmpty(), "Expected empty list when no songs are played recently");
-        } else {
-            softAssert.assertTrue(recentlyPlayedSongs.getSongIds().size() <= count,
-                    "The number of songs should not exceed the requested count");
-            softAssert.assertTrue(recentlyPlayedSongs.getSongIds().stream().allMatch(id -> id.matches("[a-f0-9]{32}")),
-                    "All song IDs should be valid 32-character alphanumeric strings");
-        }
-
-        // Assert all soft assertions
-        softAssert.assertAll();
-    }
-
-
-}
-
- */
-
 package apiKoel;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -111,7 +32,7 @@ public class RecentlyPlayedSongsTest {
     public void testGetRecentlyPlayedSongs() {
         SoftAssert softAssert = new SoftAssert();
 
-        int count = 1; // Set the number of songs to be returned, if required.
+        int count = 10; // Set the number of songs to be returned, if required.
 
         // Perform GET request
         Response response = given()
